83773f4903ab86fba3d0e5096a2fef57601f7897
Dave <d.ruocco1987@googlemail.com>
2023-02-21 13:39:15 +0000

    Sets up bootcamp api endpoints

--- server.js
+++ server.js
@@ -7,6 +7,33 @@ dotenv.config({
 
 const app = express();
 
+//api endpoints
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create a new bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update or alter bootcamp ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 app.listen(
   process.env.PORT || 5000,
   console.log(
