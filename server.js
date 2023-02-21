4e3985e424ccc9ad91ac1150b4f828f794fcdcdc
Dave <d.ruocco1987@googlemail.com>
2023-02-21 13:20:07 +0000

    Initial commit, sets up project dependencies

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: "./config/config.env",
+});
+
+const app = express();
+
+app.listen(
+  process.env.PORT || 5000,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  )
+);
